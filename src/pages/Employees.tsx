56a714cc0e5823a3c0b832f517b80485a87504e5
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-05-30 08:31:23 +0300

    feat: add employees query when employees page was mounted

--- src/pages/Employees.tsx
+++ src/pages/Employees.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchEmployees } from 'store/actions/employeesActions';
 
 export const Employees = () => {
+  const employees = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchEmployees());
+  }, []);
+
   return <h1>Start Employees Task</h1>;
 };
