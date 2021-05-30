f17a690c1643802b70d63471b851c9d6b4d825bf
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-05-30 10:19:33 +0300

    refactor: create action-types instead of a case-string in a reducer

---
+++ src/store/action-types.ts
@@ -0,0 +1,3 @@
+export const FETCH_LOAD = 'FETCH_LOAD';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_ERROR = 'FETCH_LOAD';
--- src/store/reducers/employeesReducer.ts
+++ src/store/reducers/employeesReducer.ts
@@ -1,4 +1,5 @@
 import { EmployeesState } from 'interfaces/Employees';
+import { FETCH_LOAD, FETCH_SUCCESS, FETCH_ERROR } from 'store/action-types';
 
 const initialState: EmployeesState = {
   loading: false,
@@ -10,18 +11,18 @@ export const employeesReducer = (state = initialState, action: any) => {
   const { type, payload } = action;
 
   switch (type) {
-    case 'FETCH_LOAD':
+    case FETCH_LOAD:
       return {
         ...state,
         loading: true,
       };
-    case 'FETCH_SUCCESS':
+    case FETCH_SUCCESS:
       return {
         ...state,
         loading: false,
         items: payload,
       };
-    case 'FETCH_ERROR':
+    case FETCH_ERROR:
       return {
         ...state,
         loading: false,
