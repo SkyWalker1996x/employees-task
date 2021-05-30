01bd5296b8db6f6fe83e1a4d8050acc1484112f3
Chekhuta Dmytro <chehuta1996@gmail.com>
2021-05-30 20:31:02 +0300

    feat: add new util for transform employees list to month employees list;

--- src/utils/employees.ts
+++ src/utils/employees.ts
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import { Employees, TransformToAlphabetList } from 'interfaces/Employees';
 
 export const transformToAlphabetList: TransformToAlphabetList = (list, sortField) => {
@@ -24,6 +25,41 @@ export const transformToAlphabetList: TransformToAlphabetList = (list, sortField
 
     return {
       letter,
+      items: sortItems,
+    };
+  });
+
+  return transformList;
+};
+
+export const transformToMonthList = (list: Employees, sortField: string) => {
+  const monthsArray = Array.from({ length: 12 }, (e, i) => {
+    // @ts-ignore
+    return new Date(null, i + 1, null).toLocaleDateString('en', { month: 'long' });
+  });
+
+  const completeTaskMonthIndex = 4;
+  const firstPart = monthsArray.slice(0, completeTaskMonthIndex);
+  const secondPart = monthsArray.slice(completeTaskMonthIndex);
+
+  const transformList = [...secondPart, ...firstPart].map(month => {
+    const filterItems = list.filter(item => {
+      return moment(item.dob).format('MMMM') === month;
+    });
+
+    const sortItems = filterItems.sort((a, b) => {
+      // @ts-ignore
+      const sortFieldA = a[sortField];
+      // @ts-ignore
+      const sortFieldB = b[sortField];
+
+      if (sortFieldA < sortFieldB) return -1;
+      if (sortFieldA > sortFieldB) return 1;
+      return 0;
+    });
+
+    return {
+      month: month,
       items: sortItems,
     };
   });
